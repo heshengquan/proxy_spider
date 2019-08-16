d957f60432de25c334fd0efdd617ba9fc19039fd
heshengquan <651980135@qq.com>
2019-08-16 14:30:24 +0800

    创建代理池

---
+++ ips_db.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+import redis
+import proxy_pool
+
+# r = redis.Redis(host='127.0.0.1', port=6379)  # host后的IP是需要连接的ip，本地是127.0.0.1或者localhost
+pool = redis.ConnectionPool(host='127.0.0.1', port=6379, password='Wangdapao0916')
+r = redis.Redis(connection_pool=pool)  # 建立连接池避免每次连接释放消耗
+
+
+# 主ip池
+def add_ip(ip):
+    r.rpush('Iplist', ip)
+
+
+# ip数量
+def len_ip():
+    return r.llen('Iplist')
+
+
+# 取出ip
+def get_ip(num):
+    i = r.lrange('Iplist', 0, num)
+    return i
+
+
+# 删除不可用ip
+def rem_ip(ipprot):
+    i = r.lrem('Iplist', 0, ipprot)
+    return i
+
+
+# 如果ip池数量少于25个 则填满
+def act_lenip():
+    if r.llen('Iplist') < 25:
+        print('填ip')
+        while r.llen('Iplist') <= 50:
+            ip_list = proxy_pool.get_ip_list()
+            proxy_pool.inspect_ip(ip_list)
+
+
+if __name__ == '__main__':
+    act_lenip()
